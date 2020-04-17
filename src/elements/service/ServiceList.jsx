f04cfadbcdafd69cffceca2f2fb3a963b0bef377
Siobhan McKenzie <siobhan@Siobhans-MacBook-Pro.local>
2020-04-17 14:13:56 +0100

    first commit

---
+++ src/elements/service/ServiceList.jsx
@@ -0,0 +1,66 @@
+import React ,{ Component }from "react";
+import { FiCast , FiLayers , FiUsers , FiMonitor } from "react-icons/fi";
+
+const ServiceList = [
+    {
+        icon: <FiLayers />,
+        title: 'The code',
+        description: '"Keep It Simple, Stupid!" I develop a codebase which is easily testable, built to last (reusable & understable) and avoids abstraction, with a "clean code" approach. Designed to be handed over once - not again and again.'
+    },
+    {
+        icon: <FiCast />,
+        title: 'Business',
+        description: 'I develop Minimum Viable Products (MVPs), and support small and medium sized business with ongoing maintenance and feature development.'
+    },
+    {
+        icon: <FiUsers />,
+        title: 'Communication',
+        description: 'Daily communication is typical on live projects with at least 1 day/week resource required.'
+    },
+    { 
+        icon: <FiMonitor />,
+        title: 'Mobile App Development',
+        description: 'React Native is '
+    },
+    {
+        icon: <FiUsers />,
+        title: 'Marketing & Reporting',
+        description: 'I throw myself down among the tall grass by the stream as I lie close to the earth.'
+    },
+    { 
+        icon: <FiMonitor />,
+        title: 'Mobile App Development',
+        description: 'I throw myself down among the tall grass by the stream as I lie close to the earth.'
+    }
+]
+
+
+class ServiceThree extends Component{
+    render(){
+        const {column } = this.props;
+        const ServiceContent = ServiceList.slice(0 , this.props.item);
+        
+        return(
+            <React.Fragment>
+                <div className="row">
+                    {ServiceContent.map( (val , i) => (
+                        <div className={`${column}`} key={i}>
+                            <a href="/service-details">
+                                <div className="service service__style--2">
+                                    <div className="icon">
+                                        {val.icon}
+                                    </div>
+                                    <div className="content">
+                                        <h3 className="title">{val.title}</h3>
+                                        <p>{val.description}</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    ))}
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+export default ServiceThree;
